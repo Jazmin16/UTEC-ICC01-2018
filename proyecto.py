7a644ed39371c8ce078c569ce8b3cc0eb8a14313
StephanoWurttele <39769681+StephanoWurttele@users.noreply.github.com>
2018-07-02 11:28:17 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -10,7 +10,23 @@ def resta(lista):
 def promedio(lista):
     p = sum(lista)/len(lista)
     return p
-
+def mayor(lista):
+    mayor = 0
+    if lista[0]>=lista[1]:
+        if lista [0]>=lista[2]:
+            mayor = lista[0]
+        else:
+            mayor = lista[2]
+    elif lista[1]>=lista[2]:
+        mayor = lista[1]
+    else:
+        mayor = lista[2]
+    return (mayor)
+def multiplicacion(lista):
+    m = 1
+    for i in range (len(lista)):
+        m = m*lista[i]
+    return (m)
 
 for i in range(3):
     a=int(input())
